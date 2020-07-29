35c6bbc06e52504c2fbd8439302f2a2fb939f51a
tvt0 <53782830+tvt0@users.noreply.github.com>
2020-07-29 20:06:50 +0900

    2020-07-29

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,22 @@
 (function ($) {
-    // order slide
-    var mySwiper = new Swiper('.swiper-container', {
-        slidesPerView: 3.5,
-        spaceBetween: 10,
-        keyboard: {
-            enabled: true,
-            onlyInViewport: false,
-        },
+    // loading
+    $(window).load(function () {
+        $("#loading").hide();
+        $("body").removeClass("noScroll");
+    });
 
-    })
+    // side-nav
+    $("#header .ham").click(function (e) {
+        e.preventDefault();
+        $("#side-nav .side-nav").animate({
+            'left': '0'
+        }, 300);
+        $("#side-nav .backdrop").addClass("on");
+    });
+    $("#side-nav .backdrop").click(function () {
+        $("#side-nav .side-nav").animate({
+            'left': '-100%'
+        }, 300);
+        $(this).removeClass("on");
+    });
 })(jQuery);
