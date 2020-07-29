8486169fd8c2538c17608e9791c2a3f8301ef105
tvt0 <53782830+tvt0@users.noreply.github.com>
2020-07-29 19:47:33 +0900

    2020-07-29

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+(function ($) {
+    // order slide
+    var mySwiper = new Swiper('.swiper-container', {
+        slidesPerView: 3.5,
+        spaceBetween: 10,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+
+    })
+})(jQuery);
